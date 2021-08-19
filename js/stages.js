0affe253aa276abb6eb2e37a1efe5a0661324968
Rian Ribeiro Rodrigues <rianribeiroprogramador@gmail.com>
2021-08-19 18:51:39 -0300

    57:40

---
+++ js/stages.js
@@ -0,0 +1,53 @@
+let stages = [
+  {
+    title: 'VEREADOR',
+    numbers: 5,
+    candidates: [
+      {
+        number: 38111,
+        name: 'Fulano de Tal',
+        party: 'ABC',
+        photos: [
+          { url: './img/alderman.png', label: 'Vereador', small: false }
+        ]
+      },
+
+      {
+        number: 77222,
+        name: 'Beltrano da Tal',
+        party: 'DEFG',
+        photos: [
+          { url: './img/mayor.png', label: 'Vereador' }
+        ]
+      },
+    ]
+  },
+
+  {
+    title: 'PREFEITO',
+    numbers: 2,
+    candidates: [
+      {
+        number: 99,
+        name: 'Ciclano',
+        party: 'ABC',
+        vice: 'Cic',
+        photos: [
+          { url: './img/mayor.png', label: 'Prefeito' },
+          { url: './img/vice-mayor.png', label: 'Vice-Prefeito', small: true },
+        ]
+      },
+
+      {
+        number: 84,
+        name: 'Zulano',
+        party: 'QWERTY',
+        vice: 'Zul',
+        photos: [
+          { url: './img/vicemayor.png', label: 'Prefeito' },
+          { url: './img/mayor.png', label: 'Vice-Prefeito', small: true },
+        ]
+      },
+    ]
+  }
+]
